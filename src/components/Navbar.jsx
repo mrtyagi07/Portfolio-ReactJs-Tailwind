93af541abb2d9d7cbef01a5dc2cdd3713d761c26
Vaibhav Tyagi <VAIBHAVTYAGI438@GMAIL.COM>
2022-11-18 02:48:18 +0530

    portfolio-done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import {
   FaLinkedin,
   FaTwitter,
 } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
+
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "../assets/vtt.png";
 import { Link } from "react-scroll";
@@ -18,7 +18,7 @@ const Navbar = () => {
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
-        <img src={Logo} alt="Logo Image" style={{ width: "200px" }} />
+        <img src={Logo} alt="Logo" style={{ width: "200px" }} />
       </div>
 
       {/*  MENU */}
